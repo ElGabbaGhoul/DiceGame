99b1fdcedfe675fa290df6b8db11fda56c1f0409
Marble <scnideffer@gmail.com>
2023-09-02 14:50:51 -0700

    Added better rolling logic and fabulous prizes

--- dice.cs
+++ dice.cs
@@ -1,8 +1,19 @@
-Random dice = new Random();
+// Random Rolls
 
+Random dice = new Random();
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
+
+// Test Doubles
+
+// int roll1 = 6;
+// int roll2 = 6;
+// int roll3 = 5;
+
+// Test Triples
+
+// int roll3 = 6;
 
 int total = roll1 + roll2 + roll3;
 
@@ -20,12 +31,27 @@ else if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
   total += 2;
 }
 
-if (total >= 15)
+if (total == 24)
 {
-  Console.WriteLine($"You rolled a {total}, you win!");
+  Console.WriteLine($"UNBELIEVABLE! YOU ROLLED A {total}! YOU WIN $10,000,000,000!!!!!!!");
 }
-
-if (total < 15)
+else if (total > 19)
 {
-  Console.WriteLine($"You rolled a {total}, sorry, you lose.");
+  Console.WriteLine($"You rolled a {total}! You win $1,000,000!");
+}
+else if (total >= 16)
+{
+  Console.WriteLine($"You rolled a {total}! You win a new car!");
+}
+else if (total >= 10)
+{
+  Console.WriteLine($"You rolled a {total}! You win a new laptop!");
+}
+else if (total == 7)
+{
+  Console.WriteLine($"You rolled a {total}! You win a trip for two!");
+}
+else
+{
+  Console.WriteLine($"You rolled a {total}! You win a kitten!");
 }
